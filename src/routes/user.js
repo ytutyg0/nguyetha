e81f19f501dec0f2069ede5fc9870452f972cd3f
vnd <vnd812@gmail.com>
2023-03-26 13:32:17 +0700

    fix bug

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,20 +1,19 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const { check, validationResult } = require('express-validator');
+const passport = require("passport");
+const { check, validationResult } = require("express-validator");
 
-const UserController = require('../controllers/userController');
+const UserController = require("../controllers/userController");
 
-router.put('/user-profile/:id',[
-    check('phone', 'Invalid phone number! must have 8-15 digit').isLength({min:8})
-  ], UserController.saveProfile);
-router.get('/user-profile', UserController.userProfile);
-router.get('/order', UserController.displayorder);
-router.post('/order', UserController.order);
-router.get('/checkout', UserController.checkout);
-router.put('/cancelOrder/:id', UserController.cancelOrder);
-
-
+router.put(
+  "/user-profile/:id",
+  [check("phone", "Số điện thoại không hợp lệ").isLength({ min: 8 })],
+  UserController.saveProfile
+);
+router.get("/user-profile", UserController.userProfile);
+router.get("/order", UserController.displayorder);
+router.post("/order", UserController.order);
+router.get("/checkout", UserController.checkout);
+router.put("/cancelOrder/:id", UserController.cancelOrder);
 
 module.exports = router;
